2a150177e525728b1c68aea2a3a27fa9ee34c243
repl.it user <replituser@example.com>
2020-10-19 17:15:55 +0000

    new try

--- heap.c
+++ heap.c
@@ -52,7 +52,7 @@ void heap_pop(Heap* pq)
 {
   if(pq == NULL) return;
 
-  heapElem* aux = NULL;
+  heapElem* aux = 0;;
   aux->data = pq->heapArray[0].data;
   aux->priority = pq->heapArray[0].priority;
 
